0938d6a5ef16631d04ef2015c9f7b65eb5ab61bc
marios-kanellopoulos <marios.kanellopoulos@vodafone.com>
2024-01-20 10:24:07 +0200

    upcoming events created

---
+++ src/components/index.tsx
@@ -0,0 +1,17 @@
+import UpcomingEvents from "./home/upcoming-events";
+import DealsChart from "./home/deals-chart";
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import ProjectCardSkeleton from "./skeleton/project-card";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+
+export {
+	UpcomingEvents,
+	DealsChart,
+	UpcomingEventsSkeleton,
+	AccordionHeaderSkeleton,
+	KanbanColumnSkeleton,
+	ProjectCardSkeleton,
+	LatestActivitiesSkeleton,
+};
