8c855bc0cf56562b80a850338f6bb560dcfeebcc
marios-kanellopoulos <marios.kanellopoulos@vodafone.com>
2024-01-21 09:21:19 +0200

    edit company functionality implemented

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ import { Home, ForgotPassword, Login, Register, CompanyList } from "./pages";
 import Layout from "./components/layout";
 import { resources } from "./config/resources";
 import Create from "./pages/company/create";
+import Edit from "./pages/company/edit";
 
 function App() {
 	return (
@@ -65,6 +66,7 @@ function App() {
 									<Route path="/companies">
 										<Route index element={<CompanyList />} />
 										<Route path="new" element={<Create />} />
+										<Route path="edit/:id" element={<Edit />} />
 									</Route>
 								</Route>
 							</Routes>
