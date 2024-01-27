25a90fbe756a3492f73dc1ce57c9966b44d52149
marios-kanellopoulos <marios.kanellopoulos@vodafone.com>
2024-01-27 09:56:43 +0200

    kanban board created

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import Layout from "./components/layout";
 import { resources } from "./config/resources";
 import Create from "./pages/company/create";
 import Edit from "./pages/company/edit";
+import List from "./pages/tasks/list";
 
 function App() {
 	return (
@@ -67,6 +68,9 @@ function App() {
 										<Route index element={<CompanyList />} />
 										<Route path="new" element={<Create />} />
 										<Route path="edit/:id" element={<Edit />} />
+									</Route>
+									<Route path="/tasks">
+										<Route index element={<List />} />
 									</Route>
 								</Route>
 							</Routes>
